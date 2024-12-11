c8b32605b9582bd26e3dff598fc2c20706e1f2a3
Prasad Gurude <prasadgurude76@gmail.com>
2024-12-11 13:53:43 +0530

    token generation procees is completed still the backend is not connected to the frontend

--- twitter-server/src/app/user/resolver.ts
+++ twitter-server/src/app/user/resolver.ts
@@ -1,4 +1,6 @@
 import axios from "axios"
+import { prismaclient } from "../../clients/db";
+import JWTService from "../../services/jwt";
 
 interface GoogleTokenResult{
   iss?:           string;
@@ -12,9 +14,9 @@ interface GoogleTokenResult{
   picture:        string;
   given_name:     string;
   family_name:    string;
+  iat:            string;
   exp:            string;
   jti:            string;  
-  iat:            string;
   alg:            string;
   kid:            string;  
   typ:            string;
@@ -25,11 +27,31 @@ const queries = {
         const googleOauthURL = new URL('https://oauth2.googleapis.com/tokeninfo')
         googleOauthURL.searchParams.set("id_token",googleToken)
 
-        const {data} =await axios.get(googleOauthURL.toString(),{
+        const {data} =await axios.get<GoogleTokenResult>(googleOauthURL.toString(),{
             responseType:"json"
         })
-        console.log(data)
-        return "ok";
+
+        const user = await prismaclient.user.findUnique({
+            where: {
+                email:data.email
+            }
+        });
+
+        if(!user){
+            const newUser = await prismaclient.user.create({
+                data:{
+                    email:data.email,
+                    firstName:data.given_name,
+                    lastName:data.family_name,
+                    profileImageURL:data.picture,
+                }                
+            })
+            const userToken = JWTService.generateTokenForUser(newUser)
+            return userToken
+        }
+
+        const userToken = JWTService.generateTokenForUser(user!)
+        return userToken;
     },
 };
 
